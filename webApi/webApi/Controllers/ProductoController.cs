d1fba7d13a12fc0e9fb428c6784d98bffdceccb9
Chris <88003200+Chrioo@users.noreply.github.com>
2022-06-08 12:49:16 -0600

    Program

---
+++ webApi/webApi/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace webApi.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult add()
+        { 
+            using (Models.Bar1Entities db = new Models.Bar1Entities())
+        
+        {
+        var oProducto = new Models.Productos();
+                oProducto.nomProd= "Energizer";
+                oProducto.descripcion = "Botella";
+                oProducto.precio = Convert.ToDecimal(1.90); 
+                oProducto.cantidad = 3;
+                oProducto.estado = 4;
+                db.Productos.Add(oProducto);
+                db.SaveChanges();
+            }
+
+            return Ok("Hecho");
+
+        }
+    }
+}
